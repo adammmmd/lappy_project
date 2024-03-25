a8ffbd16dd92e13be31e041e0e0fdebfcf4df216
adammmmd <121075246+adammmmd@users.noreply.github.com>
2024-03-25 10:19:20 +0700

    change staticfiles_dirs on settings

--- lappy_project/settings.py
+++ lappy_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'lappy_app/static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
