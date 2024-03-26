4095ffe6f22820fd0820144edf80acdc82e8bfc7
adammmmd <121075246+adammmmd@users.noreply.github.com>
2024-03-26 14:04:38 +0700

    change style

--- lappy_app/views.py
+++ lappy_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .form import *
 from .models import *
 
@@ -38,7 +39,7 @@ def contact_view(request):
             return redirect('contact')
     return render(request, "contact.html")
 
-
+# @login_required()   
 def cart_view(request):
     if request.method == "POST":
         pass
