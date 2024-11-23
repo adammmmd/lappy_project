dc079f9c5d14603cd83d47189825e0ecdba5c42e
Adam Muhammad <adam.muhammad@cs.ui.ac.id>
2024-11-23 14:01:33 +0700

    fix

--- lappy_project/settings.py
+++ lappy_project/settings.py
@@ -16,11 +16,11 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static', 
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
